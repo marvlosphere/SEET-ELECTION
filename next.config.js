263bee679f60157326b7a4b3c7a1e7e84020aa16
Bayeri Marvelous <miraclemarvelous80@gmail.com>
2026-06-27 20:48:25 +0100

    next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          { key: 'Cache-Control', value: 'no-store, no-cache, must-revalidate' },
+        ],
+      },
+    ]
+  },
+}
 module.exports = nextConfig
